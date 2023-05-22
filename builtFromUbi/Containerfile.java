af2795e74dcf34f02c45315de8a6ebf141189c41
hhue13 <hhue13@gmail.com>
2023-05-22 19:43:14 +0200

    Made UBI based image build OpenShift ready

--- builtFromUbi/Containerfile.java
+++ builtFromUbi/Containerfile.java
@@ -3,7 +3,7 @@
 ## curl -L -o hhue.json -X GET https://registry.access.redhat.com/v2/ubi9/ubi-minimal/tags/list && cat hhue.json | jq ".tags" | tr -d '" ,' | grep -Po "[0-9]{1,2}\.[0-9]{1,2}\.{0,1}[0-9]{0,2}-{0,1}[0-9]{0,4}$" |sort | tail -1
 ##
 ## Sample build command:
-## time podman build -f Containerfile.java11 -t localhost/itrzzs/java11:$(date +"%Y.%V") --build-arg REL_BASEIMAGE="registry.redhat.io/ubi9/ubi-minimal:9.2-484" --build-arg JAVA_VERSION="java-17-openjdk" . 2>&1
+## time podman build -f Containerfile.java -t localhost/itrzzs/java11:$(date +"%Y.%V") --build-arg REL_BASEIMAGE="registry.redhat.io/ubi9/ubi-minimal:9.2-484" --build-arg JAVA_VERSION="java-11-openjdk" . 2>&1
 ##
 ## Note on the registry:
 ## ====================
